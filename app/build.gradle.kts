b31c86293f3e5345e3b1f7aa6ac9dca52dd3bca9
dttHuongg <huong.dinhtthuy203@gmail.com>
2023-10-31 13:37:06 +0700

    Máactivity_má»ore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.mychat"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mychat"
--- app/src/main/java/com/example/mychat/MainActivity.java
+++ app/src/main/java/com/example/mychat/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.mychat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] items = new String[]{"Account", "Chats", "Apperance", "Notification", "Privacy", "Data Usage", "Help", "Invite Your Friends"};
+    Integer[] icons={R.drawable.ic_avt,R.drawable.ic_chats, R.drawable.ic_apperance, R.drawable.ic_noti, R.drawable.ic_privacy, R.drawable.ic_data, R.drawable.ic_help, R.drawable.ic_invite };
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_more);
+        listView = findViewById(R.id.listView);
+
+        CustomListMore adapter = new CustomListMore(this, R.layout.custom_listview_more, items, icons);
+// bind intrinsic ListView to custom adapter
+        listView.setAdapter(adapter);
     }
+
 }
